f0409e3e955330066e5de1c01277c4d278675fe1
Anton Griev <anton@griev.ru>
2015-07-11 17:44:51 +0300

    test of data render

--- BCApp/views.py
+++ BCApp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 # Create your views here.
+from BCApp.models import BCBusinessUnit,BCBusinessModel,BCUser
 
 def home(request):
     context = { "name" : "name"}
     return render(request, "BCApp/home.html", context)
+
+def model_list(request):
+    models = BCBusinessModel.objects.all()
+    context = { "models" : models}
+    return render(request, "BCApp/models_list.html", context)
