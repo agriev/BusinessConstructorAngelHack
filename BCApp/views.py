0042d171813c7f393221e1b7c4dbddf45797cf98
Anton Griev <anton@griev.ru>
2015-07-11 17:27:25 +0300

    bootstrap template2

--- BCApp/views.py
+++ BCApp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render_to_response
 # Create your views here.
 
 def home(request):
-
-    render_to_response(request, "BCApp/home.html", {})
+    context = { "name" : "name"}
+    return render(request, "BCApp/home.html", context)
